eabdd1258b0c13716dc26ea917f77688c29d596c
2018lopez <2018119251@ub.edu.bz>
2022-04-06 12:17:16 -0600

    update mms.test

--- code_1/mms.test.js
+++ code_1/mms.test.js
@@ -10,9 +10,15 @@ const manageUser = new management()
 const vendorUser = new vendor()
 
 // Pass Test for UC-2: ViewMarketMap
-test('Select Market to view grid Map - UC-2: ViewMarketMap', () =>{
+test('Select Market to view grid Map - UC-2: ViewMarketMap ', () =>{
 
     expect(user.viewMarketMap('Belmopan')).toBe(true)
+})
+
+// Pass fail for UC-2: ViewMarketMap
+test('Select Market to view grid Map - UC-2: ViewMarketMap', () =>{
+
+    expect(user.viewMarketMap('Stann Creek')).toBe(true)
 })
 
 
@@ -23,10 +29,22 @@ test('Get Route - UC-3: GetMarketRoute ', () =>{
 
 })
 
+// Fail Test for UC-3: GetMarketRoute
+test('Get Route - UC-3: GetMarketRoute ', () =>{
+
+    expect(user.getMarketRoute('17.1573248','-89.0830848','17.1569824,','-89.0730777')).toBe(true)
+
+})
 // Pass Test for UC-4: TourMarket
 test('Tour Market - UC-4: TourMarket',() =>{
 
     expect(user.tourMarket('San Ignacio')).toBe(true)
+})
+
+// Fail Test for UC-4: TourMarket
+test('Tour Market - UC-4: TourMarket',() =>{
+
+    expect(user.tourMarket('Corozal')).toBe(true)
 })
 
 // Pass Test for  UC-5: TourStall
@@ -35,10 +53,22 @@ test('Tour Stall - UC-5: TourStall',() =>{
     expect(user.tourStall('C10')).toBe(true)
 })
 
+// Fail Test for  UC-5: TourStall
+test('Tour Stall - UC-5: TourStall',() =>{
+
+    expect(user.tourStall('C105')).toBe(true)
+})
+
 // Pass Test for UC-7: BuyItem
 test('Buy Item - UC-7: BuyItem ', () =>{
     
     expect(user.buyItem(1002, 1001, 2)).toBe(true)
+})
+
+// Fail Test for UC-7: BuyItem
+test('Buy Item - UC-7: BuyItem ', () =>{
+    
+    expect(user.buyItem(1002, 10011, 2)).toBe(true)
 })
 
 // Pass Test for UC-1: AuthVendorandManagement
@@ -48,6 +78,12 @@ test('Auth Vendor and Management Users - UC-1: AuthVendorandManagement',() =>{
     expect(auth('ilopez','%34k','vendor')).toBe(true)
 })
 
+// Fail Test for UC-1: AuthVendorandManagement
+test('Auth Vendor and Management Users - UC-1: AuthVendorandManagement',() =>{
+    
+    
+    expect(auth('ilopez','%34k','management')).toBe(true)
+})
 
 // Pass Test for UC-11: GenerateReport
 
@@ -66,6 +102,13 @@ test('ApproveReservedStall - UC-15: ApproveReservedStall',() =>{
     expect(manageUser.approveReserveStall('v32','P15',auth('rRamos','%34k','management'))).toBe(true)
 })
 
+// Fail Test for UC-15: ApproveReservedStall
+
+test('ApproveReservedStall - UC-15: ApproveReservedStall',() =>{
+    
+    
+    expect(manageUser.approveReserveStall('v32','P5',auth('rRamos','%34k','management'))).toBe(true)
+})
 
 
 // Past Test UC-18: GenerateInvoices
